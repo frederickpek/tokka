03b9d64e3ca63e088e98b4e7aa850352e7b8d2e2
Frederick Pek <ssixthlhm@gmail.com>
2024-09-29 01:35:12 +0800

    add aioredis client

--- loader/LoaderProgram.py
+++ loader/LoaderProgram.py
@@ -1,24 +1,37 @@
 import asyncio
+from util.RedisClient import RedisClient
 from loader.TransactionLoader import TransactionLoader
 from loader.TransactionVerifier import TransactionVerifier
 
 
 class LoaderProgram:
     def __init__(self):
+        self.redis = RedisClient()
         self.loader = TransactionLoader()
         self.verifier = TransactionVerifier()
         self.refresh_sec = 5
 
+    async def process_invalid_hash(hash: str):
+        pass
+
+    async def process_valid_hash(hash: str, transaction: dict):
+        pass
+
+    async def process_hash(self, hash: str):
+        transaction = await self.loader.get_transaction(hash)
+        valid_transaction = await self.verifier.verify_transaction(transaction)
+        if valid_transaction:
+            print(f"valid transaction -- {hash}")
+            await self.process_valid_hash(hash)
+        else:
+            print(f"invalid transaction -- {hash}")
+            await self.process_invalid_hash(hash)
+
     async def loop(self):
         while True:
             try:
-                hash = ""
-                transaction = await self.loader.get_transaction(hash)
-                valid_transaction = await self.verifier.verify_transaction(transaction)
-                if valid_transaction:
-                    print("valid transaction")
-                else:
-                    print("invalid transaction")
+                hash = "0x0"
+                self.process_hash(hash)
             except Exception as err:
                 pass
             await asyncio.sleep(self.refresh_sec)
---
+++ util/RedisClient.py
@@ -0,0 +1,32 @@
+import json
+from aioredis import Redis
+
+
+class RedisClient(Redis):
+    async def hset_json(self, name, key, value):
+        return await self.hset(name, key, json.dumps(value))
+
+    async def hget_json(self, name, key):
+        obj = await self.hget(name, key)
+        return json.loads(obj) if obj is not None else obj
+
+    async def hgetall_json(self, name):
+        obj = await self.hgetall(name)
+        return {
+            k.decode("utf-8")
+            if isinstance(k, bytes)
+            else k: json.loads(v)
+            if v is not None
+            else v
+            for k, v in obj.items()
+        }
+
+    async def set_key(self, name, value, expire: int = None) -> bool:
+        if self._readonly:
+            return
+        res = await self.set(name, value, ex=expire)
+        return res
+
+    async def get_key(self, name):
+        result = await self.get(name)
+        return result.decode("utf-8") if isinstance(result, bytes) else result
