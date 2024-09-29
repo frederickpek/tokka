2cbbde4d1dbd68e64d29912fd578d55ed6755932
Frederick Pek <ssixthlhm@gmail.com>
2024-09-29 21:53:07 +0800

    remove unused methods

--- util/RedisClient.py
+++ util/RedisClient.py
@@ -14,23 +14,5 @@ class RedisClient(Redis):
         obj = await self.hget(name, key)
         return json.loads(obj) if obj is not None else obj
 
-    async def hgetall_json(self, name):
-        obj = await self.hgetall(name)
-        return {
-            k.decode("utf-8")
-            if isinstance(k, bytes)
-            else k: json.loads(v)
-            if v is not None
-            else v
-            for k, v in obj.items()
-        }
-
-    async def set_key(self, name, value, expire: int = None) -> bool:
-        return await self.set(name, value, ex=expire)
-
-    async def get_key(self, name):
-        result = await self.get(name)
-        return result.decode("utf-8") if isinstance(result, bytes) else result
-
     async def publish_to_channel(self, channel: str, msg):
         return await self.publish(channel, json.dumps(msg))
